3e93debe1e1bde1509e7a68fe288a801a9217a69
eeropomell <pomelleero@gmail.com>
2024-12-17 05:05:12 +0200

    add 2 new jobs to AddStrokeIds and Mesh2Stroke
    Code is not clean right now, but it works, and also we're still prototyping

---
+++ UnitySDK/Assets/TiltBrush/Scripts/Jobs/AddStrokeIdsJob.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Mathematics;
+
+
+
+[BurstCompile]
+public struct AddStrokeIdsJob : IJobParallelFor
+{
+
+    [ReadOnly] public NativeArray<Vector3> uv2;
+
+    public NativeArray<float> allTimestamps;
+
+    public void Execute(int index)
+    {
+        allTimestamps[index] = uv2[index].x;
+    }
+}
+
+
+// we need to calculate the amount of vertices that each strokeID has
+// we then store that in uv3.z
+[BurstCompile]
+public struct CalculateVertexCountForEachStrokeID : IJobParallelFor
+{
+
+    // this is where we read the timestamp (strokeID) data for each vertex
+    [ReadOnly] public NativeArray<Vector3> uv2;
+
+    [ReadOnly] public NativeArray<float> strokeIDs;
+
+    // Output: Vector2 of (strokeID,vertexCount)
+    public NativeArray<Vector2> strokeData;
+
+    public void Execute(int index)
+    {
+        float strokeID = strokeIDs[index];
+
+        float2 v = strokeData[index];
+        v.x = strokeID;
+
+        int vertexCount = 0;
+        for (int i = 0; i < uv2.Length; i++)
+        {
+            if (AreFloatsEqual(uv2[i].x, strokeID))
+            {
+                vertexCount++;
+            }
+        }
+
+        v.y = vertexCount;
+
+        strokeData[index] = v;
+    }
+
+    private static bool AreFloatsEqual(float a, float b, float epsilon = 0.00001f)
+    {
+        return math.abs(a - b) < epsilon;
+    }
+}
+
+// this job will take the timestamps set and uv2, and create uv3
+[BurstCompile]
+public struct AddStrokeIdsJob2 : IJobParallelFor
+{
+
+    [ReadOnly] public NativeArray<Vector3> uv2;
+
+    [ReadOnly] public NativeArray<Vector2> strokeData;
+
+    public NativeArray<Vector3> uv3;
+
+    // we execute for each vertex
+    public void Execute(int index)
+    {
+        float3 v = uv2[index];
+        v.z = 0;
+        if (index <= strokeData.Length - 1)
+        {
+            v.y = strokeData[index].x;
+            v.z = strokeData[index].y;
+        }
+        else
+        {
+            v.y = 0;
+        }
+
+        uv3[index] = v;
+    }
+
+
+}
+
+
+
+
+
+
+
+
---
+++ UnitySDK/Assets/TiltBrush/Scripts/Jobs/CreateVertexArraysJob.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Mathematics;
+
+
+[BurstCompile]
+public struct CreateVertexArraysJob : IJobParallelFor
+{
+
+    [ReadOnly] public NativeArray<float> strokeIDs;
+
+    [ReadOnly] public NativeArray<Vector3> uv3;
+
+    [NativeDisableParallelForRestriction]
+    public NativeArray<int> vertexMap;
+
+    public void Execute(int index)
+    {
+        float strokeId = strokeIDs[index];
+
+        int startingVertexIndex = 0;
+
+        bool lookingForStartingIndex = true;
+
+
+        for (int i = 0; i < uv3.Length; i++)
+        {
+            if (lookingForStartingIndex)
+            {
+                if (AreFloatsEqual(uv3[i].x, strokeId))
+                {
+                    startingVertexIndex = i;
+                    lookingForStartingIndex = false;
+                }
+            }
+            else
+            {
+                // we're done
+                if (!AreFloatsEqual(uv3[i].x, strokeId))
+                {
+                    break;
+                }
+
+                vertexMap[i] = i - startingVertexIndex;
+            }
+        }
+
+
+    }
+
+    private static bool AreFloatsEqual(float a, float b, float epsilon = 0.00001f)
+    {
+        return math.abs(a - b) < epsilon;
+    }
+}
